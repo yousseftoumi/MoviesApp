2dfcc1cf7cec9fc96213ddaa0d5784efc8bbb9e5
YoussefToumi <yousseftoumi42@gmail.com>
2022-04-25 23:03:16 +0000

    first commit

---
+++ API/TMDBApi.js
@@ -0,0 +1,13 @@
+const API_TOKEN = "4bff00d926ad7b0476a0e796e59b065f";
+export function getFilmsFromApiWithSearchedText (text, page) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
+    return fetch(url)
+      .then((response) => response.json())
+      .catch((error) => console.error(error))
+
+  }
+  // API/TMDBApi.js
+
+export function getImageFromApi (name) {
+    return 'https://image.tmdb.org/t/p/w300' + name
+  }
